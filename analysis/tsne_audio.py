840a098700b7d48f48df3f3d0543c6254b781903
fabian sguiglia <fsguiglia@gmail.com>
2021-08-09 10:06:25 -0300

    dimensionality reduction for audio files

---
+++ analysis/tsne_audio.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+import numpy as np
+import librosa
+
+import progressbar
+
+import os
+import easygui
+import argparse
+import sys
+import json
+
+def main():
+	args = process_arguments(sys.argv)
+	
+	if args['input'] is None:
+		init_path = os.environ['USERPROFILE'] + '//Desktop//'
+		args['input'] = (easygui.diropenbox(default = init_path))
+		if args['input'] is None:
+			exit()
+
+	files = getListOfFiles(args['input'], ['.wav'])
+	
+	
+	if len(files) < 5:
+		exit()
+	
+	sample_rate = args['sample_rate']
+	window_size = args['window_size']
+	hop_length = args['hop_length']
+	perplexity = args['perplexity']
+	learning_rate = args['learning_rate']
+	iterations = args['iterations']
+	mode = args['mode']
+	technique = args['technique']
+	
+	X = np.array([]).reshape(0, int(sample_rate * 0.5))
+	file_position = list()
+	
+	print('loading ' + str(len(files)) + ' files')
+	bar = progressbar.ProgressBar(maxval=len(files), \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+	bar.start()
+	for index, file in enumerate(files):
+		cur_file_position, cur_X = getFilePosition(file, sample_rate, mode)
+		file_position.append(cur_file_position)
+		X = np.concatenate((X, cur_X))
+		bar.update(index + 1)
+	bar.finish()
+	D = getFeatures(X, window_size, hop_length)
+	Y = np.zeros((len(file_position),2))
+	
+	if technique == 'pca':
+		Y = getPCA(D, 2)
+	elif technique == 'tsne':
+		pca = getPCA(D, 0.8)
+		Y = getTSNE(pca, 2, perplexity, learning_rate, iterations) 
+	
+	min_max_normalize(Y)
+	save(Y, file_position)
+	exit()
+		
+def process_arguments(args):
+	parser = argparse.ArgumentParser(description='CBCS t-SNE')
+	
+	parser.add_argument('-f', '--input',
+						action='store',
+						help='path to the input directory')
+
+	parser.add_argument('-d', '--dimentions',
+						action='store',
+						default=2,
+						help='Dimension of the embedded space (default: 2)')
+
+	parser.add_argument('-p', '--perplexity',
+						action='store',
+						default=30,
+						help='Perplexity (default: 30)')
+						
+	parser.add_argument('-l', '--learning_rate',
+						action='store',
+						default = 200,
+						help = 'Learning rate (default: 200)')
+						
+	parser.add_argument('-i', '--iterations',
+						action='store',
+						default=1000,
+						help='Iterations (default: 1000)')
+	
+	parser.add_argument('-sr', '--sample_rate',
+						action='store',
+						default=22500,
+						help='Sample rate (default: 22500)')
+						
+	parser.add_argument('-ws', '--window_size',
+						action='store',
+						default=2048,
+						help='Window size (default: 2048)')
+	
+	parser.add_argument('-hl', '--hop_length',
+						action='store',
+						default=512,
+						help='Hop length (default: 512)')
+	
+	parser.add_argument('-m', '--mode',
+					 action='store',
+					 default='sample',
+					 help= 'mode (sample, granular)')
+	
+	parser.add_argument('-t', '--technique',
+					 action='store',
+					 default='tsne',
+					 help='Dimensionality reduction technique (tsne, pca)')
+
+	return vars(parser.parse_args())
+
+def getListOfFiles(dirName, extensions):
+	listOfFile = os.listdir(dirName)
+	allFiles = list()
+	for entry in listOfFile:
+		fullPath = os.path.join(dirName, entry)
+		if os.path.isdir(fullPath):
+			allFiles = allFiles + getListOfFiles(fullPath, extensions)
+		else:
+			valid_extension = False
+			for extension in extensions:
+				if fullPath[-4:] == extension:
+					valid_extension = True
+					break
+			if(valid_extension):
+				allFiles.append(fullPath)
+	return allFiles
+
+def getFilePosition(file, sample_rate, mode='sample'):
+	y, sr = librosa.load(file, sample_rate)
+	y = librosa.to_mono(y)
+	shape = int(sample_rate * 0.5)
+
+	windows = int(y.shape[0] / shape)
+	if mode == 'sample': 
+		windows = 0
+	X = list()
+	D = np.array([]).reshape(0, shape)
+	
+	for i in range(windows + 1):
+		start = int(shape * i)
+		end = int(shape * (i + 1))
+		if end > y.shape[0]: 
+			end = y.shape[0]
+		X.append((file, i * 500))
+		padded = np.zeros(shape)
+		padded[:end] = y[start:end]
+		padded = padded.reshape(1, shape)
+		D = np.concatenate((D,padded))
+	
+	return X, D
+	
+def getFeatures(samples, window_size, hop_length):
+	shape = int((1 + window_size * 0.5))
+	shape *= int(samples[1].shape[0] / hop_length) + 1
+	D = np.array([]).reshape(0, shape)
+	for sample in samples:
+		sample = sample.T
+		S = librosa.stft(sample, n_fft = window_size, hop_length = hop_length)
+		S = np.abs(S)
+		S = S.reshape(S.shape[0] * S.shape[1])
+		S = S.reshape(1, shape)
+		D = np.concatenate((D, S))
+	return D
+
+def getPCA(data, components):
+	print('Principal component analysis')
+	pca = PCA(n_components=components)
+	pca.fit(data)
+	Y = pca.transform(data)
+	print('done, ' + str(Y.shape[1]) + ' components')
+	return Y
+
+def getTSNE(data, components, perplexity, learning_rate, iterations):
+	print('t-distributed stochastic neighbor embedding')
+	tsne = TSNE(n_components = components,
+			 perplexity = perplexity,
+			 learning_rate = learning_rate,
+			 n_iter = iterations,
+			 verbose = 1,
+			 )
+	Y = tsne.fit_transform(data)
+	print('done!')
+	return Y
+
+def min_max_normalize(a):
+	a = np.transpose(a)
+	max_values = np.amax(a,1)
+	min_values = np.amin(a,1)
+	a = np.transpose(a)
+	norm = (a - min_values) / (max_values - min_values)
+	return norm
+
+def save(data, files):
+	out = dict()
+	for i in range(len(files)):
+		curOut = dict()
+		curOut['file'] = files[i][0][0]
+		curOut['pos'] = files[i][0][1]
+		curOut['x'] = float(data[i][0])
+		curOut['y'] = float(data[i][1])
+		out[i] = curOut
+	
+	with open('test.json', 'w+') as f:
+		json.dump(out, f, indent = 4)
+		
+		
+	
+
+main()
